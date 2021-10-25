1114951963844302494102b961fef3a396c6e8cd
MohamadJavadMoghofei <81424606+thisJavad98@users.noreply.github.com>
2021-10-25 13:41:45 +0330

    set login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,30 @@
 import classes from "./login.module.css";
-import { Grid } from "@material-ui/core";
+import { Grid, IconButton, TextField, Typography } from "@material-ui/core";
+import { useEffect } from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { BiArrowBack } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  useEffect(() => {
+    document.title = "Login";
+  }, []);
   return (
     <>
       <Grid className={classes.layout} container lg={12}>
-        Login Page
+        <Link to="/">
+          <div className={classes.backIcon}>
+            <IconButton>
+              <BiArrowBack />
+            </IconButton>
+          </div>
+        </Link>
+        <Grid className={classes.card} container lg={4} md={4} xs={10}>
+          <div className={classes.icon}>
+            <FaUserCircle />
+          </div>
+          <TextField label="User Name" color="white" variant="outlined" />
+        </Grid>
       </Grid>
     </>
   );
