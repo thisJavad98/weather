86442061b2c1d70f0e474768373eb156f73050b1
mj407382 <mj407382@gmail.com>
2022-08-22 17:02:59 +0430

    update main page

--- src/components/weatherMain/WeatherMain.tsx
+++ src/components/weatherMain/WeatherMain.tsx
@@ -1,7 +1,26 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import { useEffect, useState } from "react";
 
 const WeatherMain = () => {
-  return <>main</>;
+  return (
+    <Box>
+      <Typography
+        fontSize={"xx-large"}
+        marginBottom={"1rem"}
+        fontFamily={"Raleway"}
+        textAlign={"center"}
+      >
+        Weather App
+      </Typography>
+      <Box
+        width={"35rem"}
+        height={"38rem"}
+        borderRadius={2}
+        bgcolor={"#F0F0F0"}
+      ></Box>
+    </Box>
+  );
 };
 
 export default WeatherMain;
